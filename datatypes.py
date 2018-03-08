20ec3c4600b0c3a190ffc853c31066ee06e2ef5f
kdcool10 <37182614+kdcool10@users.noreply.github.com>
2018-03-08 19:07:42 +0530

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,23 @@
+
+x=5 #integer
+y=23.64747 # floating point
+z='kuldeep' # string
+a=[2,3,4] #array
+b=[2,'3',[1,2], 3.46788] #list
+animals={'dog' : 2, 'rabbit' : 4, 'hamster' : 12} # dictionary
+d=(1,2,3) #tuple(value assigned in it is constant)
+
+
+
+print(a[0])
+print(b[2])
+b[2]=2342
+print(b[2])
+print(animals['hamster'])
+
+print(animals.keys())
+print(animals.values())
+
+print(d[1])
+d[1]=4
+print(d[1])
